9c4b6289ef09f813118b419aa61fde221c430ca3
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-06-09 16:24:38 +0300

    Build and run a Flask application on your computer

--- server/app.py
+++ server/app.py
@@ -1,8 +1,37 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_param(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    result = ''
+    for i in range(param):
+        result += f"{i}\n"
+    return result
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        return "Invalid operation", 400
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
